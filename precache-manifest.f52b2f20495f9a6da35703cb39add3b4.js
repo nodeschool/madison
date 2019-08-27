09084cf3572c89f79ef75dd2e62190922df692d1
Kyle Thomas <kylet.codes@gmail.com>
2019-08-27 16:05:32 -0500

    Updates

---
+++ precache-manifest.f52b2f20495f9a6da35703cb39add3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d4fa2cc3984836c8220f0f1746691d",
+    "url": "/madison/index.html"
+  },
+  {
+    "revision": "6152a959e9309861d619",
+    "url": "/madison/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c1d6b0c0f648fb7a374f",
+    "url": "/madison/static/js/2.35348412.chunk.js"
+  },
+  {
+    "revision": "6152a959e9309861d619",
+    "url": "/madison/static/js/main.8ed52995.chunk.js"
+  },
+  {
+    "revision": "2f0540c7ee1dec12d080",
+    "url": "/madison/static/js/runtime~main.e41c273f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/madison/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00119765c76788e73b9f05a54fafbd6f.js"
+  "/madison/precache-manifest.f52b2f20495f9a6da35703cb39add3b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/madison/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
