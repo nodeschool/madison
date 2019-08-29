5a9c56e2df44187c57ae627602c5077ce398662b
Kyle Thomas <kylet.codes@gmail.com>
2019-08-29 14:00:54 -0500

    Updates

---
+++ precache-manifest.442f15b343198340047430cb95e8f583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f1c76518e8272c06b5f78f1510bae0",
+    "url": "/madison/index.html"
+  },
+  {
+    "revision": "148cd3513b7e67d1c4c3",
+    "url": "/madison/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a095391280b0cc1ae10a",
+    "url": "/madison/static/css/main.c982a2bb.chunk.css"
+  },
+  {
+    "revision": "148cd3513b7e67d1c4c3",
+    "url": "/madison/static/js/2.8b4281e8.chunk.js"
+  },
+  {
+    "revision": "a095391280b0cc1ae10a",
+    "url": "/madison/static/js/main.3a4389b0.chunk.js"
+  },
+  {
+    "revision": "2f0540c7ee1dec12d080",
+    "url": "/madison/static/js/runtime~main.e41c273f.js"
+  },
+  {
+    "revision": "4db9b2ad5cce0bae4ea83eb79cb42561",
+    "url": "/madison/static/media/nodeschool.4db9b2ad.png"
+  }
+]);
