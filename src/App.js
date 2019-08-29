80829aa77dabd1fa8a5433d35355b68dd019b6eb
Kyle Thomas <kylet.codes@gmail.com>
2019-08-29 10:14:46 -0500

    Init to react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>NodeSchool Madison is currently under construction!</p>
+        <p>
+          Follow <a href='https://twitter.com/NodeschoolMSN'>@nodeschoolmsn</a>{" "}
+          for updates
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
